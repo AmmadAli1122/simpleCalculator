1670b2f4beace68238800f80e813b06c02bace83
Ammad Ali <aaammadali376@gmail.com>
2024-03-20 20:07:00 +0500

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 const ans = await inquirer.prompt([
     { message: "Enter First Number:\n", type: "number", name: "num1" },
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-
+#! /usr/bin/env node
 import inquirer from "inquirer";
 
 const ans = await inquirer.prompt([
